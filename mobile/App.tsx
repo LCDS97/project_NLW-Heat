e8d5bd3903da49a6a4f41ab22b53b5ffb3f85f46
LCDS97 <lucas.cds1997@gmail.com>
2021-10-23 08:50:08 -0300

    Imported custom fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { 
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
-  return (
-    <Home />
-  );
+
+    const [fontsLoaded] = useFonts({
+      Roboto_400Regular,
+      Roboto_700Bold
+    });
+
+    if(!fontsLoaded) {
+      return <AppLoading />
+    }
+
+    return (
+      <Home />
+    )
 }
 
 
