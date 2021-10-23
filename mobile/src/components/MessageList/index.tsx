23ab67ea6d60e3edfb5f6552130b2d6b1ca312cd
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-10-23 20:09:13 -0300

    Messages and list of messages done

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -8,15 +8,26 @@ import { Message } from '../Message'
 import { styles } from './styles';
 
 export function MessageList(){
+
+  const message = {
+    id: 'string',
+    text: 'mensagem',
+    user: {
+      name: 'lucas',
+      avatar_url: 'https://github.com/lcds97.png'
+    }
+  }
+
   return (
     <ScrollView 
     style={styles.container}
     contentContainerStyle={styles.content}
     keyboardShouldPersistTaps="never"
     >
-        <Message />
-        <Message />
-        <Message />
+        <Message data={message}/>
+        <Message data={message}/>
+        <Message data={message}/>
+
     </ScrollView>
   );
 }
